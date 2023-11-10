e9122b9e60285cb75bfa9ff94b4489a0ddfbe653
Angel Lopez <aabrahamlopez@gmail.com>
2023-11-10 08:59:17 -0600

    Ampliar con View Transitions

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,12 @@
 document
-    .querySelector(".hamburger")
-    .addEventListener("click", () => {
-        document
-            .querySelector(".nav-links")
-            .classList.toggle("expanded");
-    });
+    .addEventListener(
+        'astro:page-load', () => {
+            document
+                .querySelector(".hamburger")
+                .addEventListener("click", () => {
+                    document
+                        .querySelector(".nav-links")
+                        .classList.toggle("expanded");
+                });
+        }
+    );
