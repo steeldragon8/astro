69b1166e2b2dca5f3383d0f2bcbe7b5a00cce258
Angel Lopez <aabrahamlopez@gmail.com>
2023-11-10 10:39:02 -0600

    Ampliar con contenido de coleeciones

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,22 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+export async function get() {
+
+    const posts = await getCollection('posts');
+
     return rss({
         title: 'Alumno de Astro | Blog',
         description: 'Mi viaje de aprendizaje de Astro',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: posts.map(
+            (post) => ({
+                title: post.data.title,
+                pubDate: post.data.pubDate,
+                description: post.data.description,
+                link: `/blog/${post.slug}/`,
+            })
+        ),
         customData: `<language>en-us</language>`,
     });
 }
